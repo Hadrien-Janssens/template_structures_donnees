0df295c8a57736cf7cb1170c5a2a2253bd9ef052
BlackPigCompany <86426768+BlackPigCompany@users.noreply.github.com>
2024-10-02 16:48:58 +0200

    wip

--- src/Lists/LinkedList.php
+++ src/Lists/LinkedList.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Opmvpc\StructuresDonnees\Lists;
 
+use Error;
+use Exception;
 use Opmvpc\StructuresDonnees\Node;
 
 class LinkedList implements ListInterface
@@ -22,26 +24,109 @@ class LinkedList implements ListInterface
         return json_encode($this->toArray(), JSON_PRETTY_PRINT);
     }
 
-    public function push(mixed $element): void {}
+    public function push(mixed $element): void
+    {
+        $newNode = new Node($element);
 
-    public function get(int $index): mixed {}
+        if ($this->head->getNext() === null) {
+            $this->head->setNext($newNode);
+            $this->size++;
+        } else {
+            $node = $this->head->getNext();
 
-    public function set(int $index, mixed $element): void {}
+            while ($node->getNext() !== null) {
+                $node = $node->getNext();
+            }
+            $node->setNext($newNode);
+            $this->size++;
+        }
+    }
 
-    public function clear(): void {}
+    public function get(int $index): mixed
+    {
+        $i = 0;
+        $node = $this->head->getNext();
+
+        while ($index !== $i) {
+            $node = $node->getNext();
+            $i++;
+        }
+        return $node->getELement();
+    }
+
+
+    public function set(int $index, mixed $element): void
+    {
+        $i = 0;
+        $node = $this->head->getNext();
+
+        if ($index <= $this->size) {
+            while ($index !== $i) {
+                $node = $node->getNext();
+                $i++;
+            }
+            $node->setElement($element);
+        } else {
+            throw new Exception();
+        }
+    }
+
+    public function clear(): void
+    {
+        $this->head->setNext(null);
+        $this->size = 0;
+    }
 
     public function includes(mixed $element): bool {}
 
     public function isEmpty(): bool {}
 
-    public function indexOf(mixed $element): int {}
+    public function indexOf(mixed $element): int
+    {
+        $node = $this->head;
+        $i = 0;
 
-    public function remove(int $index): void {}
+        while ($i <= $this->size) {
+            $node = $node->getNext();
+            if ($node->getElement() === $element) {
+                return $i;
+            }
+            $i++;
+        }
+        throw new Exception();
+    }
+
+    public function remove(int $index): void
+    {
+        $i = 0;
+        $node = $this->head->getNext();
+
+        if ($index <= $this->size) {
+            while ($index !== $i) {
+                $node = $node->getNext();
+                $i++;
+            }
+            $node->setNext(null);
+            $this->size--;
+        } else {
+            throw new Exception();
+        }
+    }
 
     public function size(): int
     {
         return $this->size;
     }
 
-    public function toArray(): array {}
+    public function toArray(): array
+    {
+        $array = [];
+        $node = $this->head->getNext();
+        while ($node !== null) {
+            $el =  $node->getElement();
+            $array[] = $el;
+            $node = $node->getNext();
+        }
+        return  $array;
+    }
 }
